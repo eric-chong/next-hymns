8c0a5546beadbb30573aa2351d9c8b3eef5798fd
Eric Chong <hoichieric@hotmail.com>
2026-01-10 22:52:26 -0800

    add children type :rabbit:

--- src/components/ContentContainer/ContentContainer.tsx
+++ src/components/ContentContainer/ContentContainer.tsx
@@ -1,6 +1,12 @@
+import { ReactNode } from "react";
+
 import styles from "./ContentContainer.module.css";
 
 console.log("styles >>>", styles);
-export default function ContentContainer({ children }) {
+export default function ContentContainer({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return <div className={styles.container}>{children}</div>;
 }
